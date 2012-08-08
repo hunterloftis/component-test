a7955602489acea273e56e12b8614cd4717501a4
Hunter Loftis <hunter@hunterloftis.com>
2012-08-07 22:46:19 -0400

    added tests

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ var express = require('express');
 var path = require('path');
 var balance = require('./lib/balance');
 
-var config = require('./package.json').publicConfig;
-
 var balance = require('./lib/balance');
 var middleware = require('./lib/middleware');
 var users = require('./lib/users');
 var dashboard = require('./lib/dashboard');
 var flash = require('./lib/flash');
 
-balance(function() {
-    var app = express();
+// Decorate express to build our app
+module.exports = function main(config) {
+  var app = express();
 
-    middleware(app, config);
-    flash(app);
-    users(app);
-    dashboard(app);
+  middleware(app, config);
+  flash(app);
+  users(app);
+  dashboard(app);
 
+  return app;
+};
+
+// Start listening if the app has been started directly
+if (require.main === module) {
+  balance(function() {
+    var config = require('./package.json').publicConfig;
+    var app = main(config);
     app.listen(config.http_port);
     console.log("Listening on", config.http_port);
-  }
-);
+  });
+}
